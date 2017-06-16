7392b386fdfc8f6475825b66bf9315de82293bcc
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-06-17 01:46:22 +0500

    Improved countdown form UI

--- tracktime.php
+++ tracktime.php
@@ -1,63 +1,65 @@
 <?php require_once("resources/functions.php"); ?>
 
 <!DOCTYPE html>
+<html lang="en">
 
-<head>
+    <head>
 
-    <title>Track Time</title>
+        <title>Track Time</title>
 
-    <link rel="stylesheet" type="text/css" href="assets/css/layout.css"/>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Time Tracking App - Login</title>
 
-    <script type='text/javascript' src='http://code.jquery.com/jquery-1.5.2.js'></script>
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/layout.css"/>
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-<div style="display: table;margin: 15% auto 0 auto;">
+        <div id="form-container">
+            <form id="timeApp" action="savetime.php" method="POST">
 
-    <form id="timeApp" action="savetime.php" method="POST" style="margin: 0;">
+                <h2 class="countdown">00:00:00</h2>
+                <input type="text" id="task" name="task" placeholder="Task Description">
+                <input type="hidden" id="time" name="time" value="">
+                <input type="button" value="SAVE" id="sendata" class="btn"/>
 
-        <h1>Stopwatch</h1>
+            </form>
 
-        <h2>
-            <time>00:00:00</time>
-        </h2>
+            <br>
 
-        <input type="text" id="task" name="task" placeholder="Please enter task description"
-               style="width: 175px; text-align: left">
+            <input type="button" value="Start" id="start" class="btn timeAppbtn"/>
+            <input type="button" value="Pause" id="pause" class="btn timeAppbtn"/>
+            <input type="button" value="Stop" id="stop" class="btn timeAppbtn"/>
+            <input type="button" value="Reset" id="reset" class="btn timeAppbtn"/>
 
-        <input type="hidden" id="time" name="time" value="">
+            <br>
+            <br>
 
-        <input type="button" value="SAVE" id="sendata"/>
+            <span id="status">READY TO START</span>
 
-    </form>
+            <br>
 
-    <br>
+            <span id="result"></span>
 
-    <input type="button" value="Start" id="start"/>
+            <br>
 
-    <input type="button" value="Pause" id="pause"/>
+        </div>
 
-    <input type="button" value="Stop" id="stop"/>
+        <!-- Javascript -->
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 
-    <input type="button" value="Reset" id="reset"/>
+        <script type="text/javascript" src="assets/js/timer.js"></script>
 
-    <br>
-    <br>
-
-    <span id="status">READY TO START</span>
-
-    <br>
-
-    <span id="result"></span>
-
-    <br>
-
-</div>
-
-<script type="text/javascript" src="assets/js/timer.js"></script>
-
-</body>
+    </body>
 
 </html>
