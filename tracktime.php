5833656e5598489bda83d599dcf585d8605c28d7
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-07-25 13:33:35 +0500

    Improve: Logout button styles

--- tracktime.php
+++ tracktime.php
@@ -63,7 +63,7 @@ if ($timeApp->is_logged_in()) {
 
             <br>
 
-            <span id="user"><a href="logout.php">LOGOUT</a></span>
+            <span id="user"><a class="logout" href="logout.php">LOGOUT</a></span>
 
         </div>
 
