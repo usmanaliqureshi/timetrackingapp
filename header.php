0049601f4530be9e8c69fd4cceb9daf7cc082535
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-05 20:55:44 +0500

    Improved Logo Text

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
     <!-- Logo -->
     <a href="index.php" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>A</b>LT</span>
+        <span class="logo-mini"><b>TT</b>A</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Admin</b>LTE</span>
+        <span class="logo-lg"><b>TimeTracking</b>App</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
