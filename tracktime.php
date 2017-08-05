cdfc1a10f4d7cf18d070685bea9f0b86ccfef387
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-05 21:07:30 +0500

    Improved Time Tracking Page

--- tracktime.php
+++ tracktime.php
@@ -15,7 +15,7 @@ if ($timeApp->is_logged_in()) {
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>AdminLTE 2 | Blank Page</title>
+        <title>Time Tracking App v<?php echo APP_VERSION; ?> - Track Time</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.7 -->
@@ -99,17 +99,7 @@ if ($timeApp->is_logged_in()) {
 
                             <span id="result"></span>
 
-                            <br>
-
-                            <span id="user"><a class="logout" href="logout.php">LOGOUT</a></span>
-
-                            <br/><br/>
-
-                            <span>Logged in as: <b><?php echo $_SESSION["user"]; ?></b></span>
-
                         </div>
-
-                        <p class="app_name">Time Tracking App v<?php echo APP_VERSION; ?></p>
 
                     </div>
 
