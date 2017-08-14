a72cbb7660e36f6f66b846c65d0f2bbd82c39d47
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-14 14:18:31 -0700

    Update: Database Dump File

--- database/timer.sql
+++ database/timer.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 14, 2017 at 11:14 PM
+-- Generation Time: Aug 14, 2017 at 11:18 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.5.24
 
@@ -34,6 +34,28 @@ CREATE TABLE IF NOT EXISTS `times` (
   `user_id` int(255) DEFAULT NULL
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `designation` varchar(255) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `designation`, `reg_date`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Usman Ali Qureshi', 'Full Stack Website Developer', '2017-08-05 09:17:45');
+
 --
 -- Indexes for dumped tables
 --
@@ -45,6 +67,12 @@ ALTER TABLE `times`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -53,6 +81,11 @@ ALTER TABLE `times`
 --
 ALTER TABLE `times`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
