adc16cc956aed8e38de842c47b74635e34ab4d40
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-21 14:34:30 +0500

    Update: Database Dump File

--- database/timer.sql
+++ database/timer.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 14, 2017 at 11:18 PM
+-- Generation Time: Aug 21, 2017 at 11:33 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.5.24
 
@@ -45,7 +45,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `email` text NOT NULL,
+  `location` text NOT NULL,
   `designation` varchar(255) NOT NULL,
+  `experience` text NOT NULL,
+  `skills` text NOT NULL,
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
@@ -53,8 +57,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `name`, `designation`, `reg_date`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Usman Ali Qureshi', 'Full Stack Website Developer', '2017-08-05 09:17:45');
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `location`, `designation`, `experience`, `skills`, `reg_date`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Usman Ali Qureshi', 'usman@usmanaliqureshi.com', 'Gujranwala, Pakistan', 'Full Stack Website Developer', '5 years', 'UI Design, Coding, Javascript, PHP, Node.js', '2017-08-05 09:17:45');
 
 --
 -- Indexes for dumped tables
