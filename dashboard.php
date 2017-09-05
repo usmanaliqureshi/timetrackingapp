cdf3f84e1b880961150e3763fabdfe53be240627
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-09-05 16:56:05 +0500

    Remove: Unnecessary Widgets from Dashboard

--- dashboard.php
+++ dashboard.php
@@ -76,31 +76,16 @@ if ($timeApp->is_logged_in()) {
                 <div class="row">
                     <div class="col-lg-3 col-xs-6">
                         <!-- small box -->
-                        <div class="small-box bg-aqua">
-                            <div class="inner">
-                                <h3>150</h3>
-
-                                <p>New Orders</p>
-                            </div>
-                            <div class="icon">
-                                <i class="ion ion-bag"></i>
-                            </div>
-                            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- ./col -->
-                    <div class="col-lg-3 col-xs-6">
-                        <!-- small box -->
                         <div class="small-box bg-green">
                             <div class="inner">
-                                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                <h3>53<sup style="font-size: 20px">hrs</sup></h3>
 
-                                <p>Bounce Rate</p>
+                                <p>Time Logged</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-stats-bars"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                            <!--a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a-->
                         </div>
                     </div>
                     <!-- ./col -->
@@ -115,7 +100,7 @@ if ($timeApp->is_logged_in()) {
                             <div class="icon">
                                 <i class="ion ion-person-add"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                            <!--a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a-->
                         </div>
                     </div>
                     <!-- ./col -->
@@ -125,12 +110,12 @@ if ($timeApp->is_logged_in()) {
                             <div class="inner">
                                 <h3>65</h3>
 
-                                <p>Unique Visitors</p>
+                                <p>Tasks Created</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-pie-graph"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                            <!--a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a-->
                         </div>
                     </div>
                     <!-- ./col -->
@@ -140,23 +125,6 @@ if ($timeApp->is_logged_in()) {
                 <div class="row">
                     <!-- Left col -->
                     <section class="col-lg-7 connectedSortable">
-                        <!-- Custom tabs (Charts with tabs)-->
-                        <div class="nav-tabs-custom">
-                            <!-- Tabs within a box -->
-                            <ul class="nav nav-tabs pull-right">
-                                <li class="active"><a href="#revenue-chart" data-toggle="tab">Area</a></li>
-                                <li><a href="#sales-chart" data-toggle="tab">Donut</a></li>
-                                <li class="pull-left header"><i class="fa fa-inbox"></i> Sales</li>
-                            </ul>
-                            <div class="tab-content no-padding">
-                                <!-- Morris chart - Sales -->
-                                <div class="chart tab-pane active" id="revenue-chart"
-                                     style="position: relative; height: 300px;"></div>
-                                <div class="chart tab-pane" id="sales-chart"
-                                     style="position: relative; height: 300px;"></div>
-                            </div>
-                        </div>
-                        <!-- /.nav-tabs-custom -->
 
                         <!-- Chat box -->
                         <div class="box box-success">
